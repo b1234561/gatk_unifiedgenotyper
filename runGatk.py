0230bb52ad5ec2444e1d2064b0047ce01ed25e55
George Asimenos <george+github@dnanexus.com>
2012-07-02 17:34:50 -0700

    Making blocking version again

--- runGatk.py
+++ runGatk.py
@@ -201,7 +201,7 @@ def extractHeader(vcfFile):
 def reduceGatk():
     t = dxpy.open_dxgtable(job['input']['tableId'])
     print "Closing Table"
-    t.close()
+    t.close(block=True)
     job['output']['simplevar'] = dxpy.dxlink(t.get_id())
 
 def checkIntervalRange(includeList, chromosome, lo, hi):
